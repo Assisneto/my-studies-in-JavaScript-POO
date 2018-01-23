968e8f16d350512e4ff8f01f6b030496db1c16e9
Assisneto <netodamasceno12@gmail.com>
2018-01-22 22:33:22 -0200

    Adição da regra de negocio utilizando POO e pratica de programação defensiva

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,33 @@
+
+
+class Negociacao {
+  constructor(data, quantidade, valor) {
+    this._data = new Date(data.getTime()); // Programação defensiva;
+    this._quantidade = quantidade;
+    this._valor = valor;
+    Object.freeze(this);
+  }
+  get data(){
+    return new Date(this._data.getTime()); // Programação defensiva
+  }
+  get quantidade(){
+    return this._quantidade;
+  }
+  get valor(){
+    return this._valor;
+  }
+  get volume(){
+    return this._quantidade * this. _valor;
+  }
+}
+
+// Teste Programação defensiva
+var hoje = new Date();
+
+var n1 = new Negociacao(hoje, 5, 700);
+
+console.log(n1.data);
+
+hoje.setDate(11);
+
+console.log(n1.data);
